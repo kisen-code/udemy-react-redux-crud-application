64de042187ca51c8ac2f58349011f8f0ee1ec73c
氣仙 <kisen@MacBook-Pro-2.local>
2020-11-16 00:34:55 +0900

    stateStudy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello,world</h1>
-    </div>
-  );
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {count:0}
+  }
+
+  handleplus = () =>{
+    console.log(this.state.count);
+    this.setState({count: this.state.count + 1});
+  }
+
+  handlemin = () => {
+    this.setState({count:this.state.count-1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count:{this.state.count}</div>
+        <button onClick = {this.handleplus}>+1</button>
+        <button onClick = {this.handlemin}>-1</button>
+      </React.Fragment>
+      )
+  }
+
 }
 
 export default App;
