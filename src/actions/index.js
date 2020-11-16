f7200e83ed7d75e5c364d40f18d4492cf218f223
氣仙 <kisen@MacBook-Pro-2.local>
2020-11-16 21:16:04 +0900

    action

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+
+
+export count increment = () =>({
+    type:INCREMENT
+})
+
+export count increment = () =>({
+    type:DECREMENT
+})
